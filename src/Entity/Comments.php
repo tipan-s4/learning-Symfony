2f77d615e3cbbd137bea9cab99df68efbfabe120
tipan <carlos.tipan.rodr@gmail.com>
2021-11-08 12:31:44 +0100

    added User

---
+++ src/Entity/Comments.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CommentsRepository::class)
+ */
+class Comments
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titulo;
+
+    /**
+     * @ORM\Column(type="string", length=1000, nullable=true)
+     */
+    private $likes;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $fecha_publicaion;
+
+    /**
+     * @ORM\Column(type="string", length=50000)
+     */
+    private $contenido;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="comment")
+    */
+    private $user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitulo(): ?string
+    {
+        return $this->titulo;
+    }
+
+    public function setTitulo(string $titulo): self
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    public function getLikes(): ?string
+    {
+        return $this->likes;
+    }
+
+    public function setLikes(?string $likes): self
+    {
+        $this->likes = $likes;
+
+        return $this;
+    }
+
+    public function getFechaPublicaion(): ?\DateTimeInterface
+    {
+        return $this->fecha_publicaion;
+    }
+
+    public function setFechaPublicaion(\DateTimeInterface $fecha_publicaion): self
+    {
+        $this->fecha_publicaion = $fecha_publicaion;
+
+        return $this;
+    }
+
+    public function getContenido(): ?string
+    {
+        return $this->contenido;
+    }
+
+    public function setContenido(string $contenido): self
+    {
+        $this->contenido = $contenido;
+
+        return $this;
+    }
+}
